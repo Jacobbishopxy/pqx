ff53cd8c0c3d2ebc7eac40d40144abd3ac347704
jacob-work <jacobbishopxy@gmail.com>
2023-05-24 09:07:41 +0800

    :hatching_chick: add `CmdExecutor`

--- pqx/src/cmd.rs
+++ pqx/src/cmd.rs
@@ -3,36 +3,39 @@
 //! date: 2023/05/22 20:49:10 Monday
 //! brief:
 
-use std::io::{BufRead, BufReader};
-use std::process::{Child, ChildStderr, ChildStdout, Command, Stdio};
+use std::future::Future;
+use std::io::{BufRead, BufReader, Read};
+use std::process::{Child, ChildStderr, ChildStdout, Command, ExitStatus, Stdio};
 
 use tokio::sync::mpsc::Sender;
 
 use crate::error::{PqxError, PqxResult};
 
-async fn stdout_lines(stdout: ChildStdout, tx: Sender<String>) -> PqxResult<()> {
-    for line in BufReader::new(stdout).lines() {
-        let line = line?;
-
-        // println!("stdout > {:?}", line);
-
-        tx.send(line)
-            .await
-            .map_err(|_| PqxError::custom("stdout_lines async send"))?;
-    }
-
-    Ok(())
+pub enum ChildStdPipe {
+    Out(ChildStdout),
+    Err(ChildStderr),
 }
 
-async fn stderr_lines(stderr: ChildStderr, tx: Sender<String>) -> PqxResult<()> {
-    for line in BufReader::new(stderr).lines() {
-        let line = line?;
+impl From<ChildStdout> for ChildStdPipe {
+    fn from(value: ChildStdout) -> Self {
+        ChildStdPipe::Out(value)
+    }
+}
 
-        // println!("stderr > {:?}", line);
+impl From<ChildStderr> for ChildStdPipe {
+    fn from(value: ChildStderr) -> Self {
+        ChildStdPipe::Err(value)
+    }
+}
+
+async fn send_child_std<R: Read>(r: R, tx: Sender<String>) -> PqxResult<()> {
+    for line in BufReader::new(r).lines() {
+        let line = line?;
+        // println!("send_child_std > {:?}", line);
 
         tx.send(line)
             .await
-            .map_err(|_| PqxError::custom("stderr_lines async send"))?;
+            .map_err(|_| PqxError::custom("send_child_std async send"))?;
     }
 
     Ok(())
@@ -65,51 +68,88 @@ pub fn gen_bash_cmd(cmd: &str) -> PqxResult<(Child, ChildStdout, ChildStderr)> {
     Ok((child, child_stdout, child_stderr))
 }
 
-pub async fn async_cmd<F>(
+pub fn gen_ssh_cmd(
+    ip: &str,
+    cmd: &str,
+    user: Option<&str>,
+) -> PqxResult<(Child, ChildStdout, ChildStderr)> {
+    // ssh -t {ip} -o StrictHostKeyChecking=no '{cmd}'
+    // ssh -t {ip} -o StrictHostKeyChecking=no 'sudo -u {user} -H sh -c "cd ~; . ~/.bashrc; {cmd}"'
+
+    let mut command = Command::new("ssh");
+    command
+        .arg("-t")
+        .arg(ip)
+        .arg("-o")
+        .arg("StrictHostKeyChecking=no");
+
+    if let Some(usr) = user {
+        command.arg(format!("sudo -u {} -H sh -c", usr)).arg(cmd);
+    }
+
+    let mut child = command
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()?;
+
+    let child_stdout = child.stdout.take().unwrap();
+    let child_stderr = child.stderr.take().unwrap();
+
+    Ok((child, child_stdout, child_stderr))
+}
+
+pub enum CmdArg<'a> {
+    Ping {
+        addr: &'a str,
+    },
+    Bash {
+        cmd: &'a str,
+    },
+    Ssh {
+        ip: &'a str,
+        cmd: &'a str,
+        user: Option<&'a str>,
+    },
+}
+
+pub fn gen_async_execution<'a, F: Fn(String)>(
     channel_buffer: usize,
-    fo: Option<(ChildStdout, F)>,
-    fe: Option<(ChildStderr, F)>,
+    pipe: ChildStdPipe,
+    f: F,
+) -> (
+    impl Future<Output = Result<(), &'a str>>,
+    impl Future<Output = Result<(), &'a str>>,
+) {
+    let (stdout_tx, mut stdout_rx) = tokio::sync::mpsc::channel(channel_buffer);
+
+    let so_tx = async move {
+        match pipe {
+            ChildStdPipe::Out(o) => send_child_std(o, stdout_tx).await.map_err(|_| "so_tx fail"),
+            ChildStdPipe::Err(e) => send_child_std(e, stdout_tx).await.map_err(|_| "se_tx fail"),
+        }
+    };
+
+    let so_rx = async move {
+        while let Some(msg) = stdout_rx.recv().await {
+            f(msg);
+        }
+        Ok(())
+    };
+
+    (so_tx, so_rx)
+}
+
+async fn exec_async_cmd<FO, FE>(
+    channel_buffer: usize,
+    fo: Option<(ChildStdout, FO)>,
+    fe: Option<(ChildStderr, FE)>,
 ) -> PqxResult<()>
 where
-    F: Fn(String) + Send + 'static,
+    FO: Fn(String),
+    FE: Fn(String),
 {
-    let chn_o = fo.map(|(child_stdout, f)| {
-        let (stdout_tx, mut stdout_rx) = tokio::sync::mpsc::channel(channel_buffer);
-
-        let so_tx = async move {
-            stdout_lines(child_stdout, stdout_tx)
-                .await
-                .map_err(|_| "so_tx fail")
-        };
-
-        let so_rx = async move {
-            while let Some(msg) = stdout_rx.recv().await {
-                f(msg);
-            }
-            Ok(())
-        };
-
-        (so_tx, so_rx)
-    });
-
-    let chn_e = fe.map(|(child_stderr, f)| {
-        let (stderr_tx, mut stderr_rx) = tokio::sync::mpsc::channel(channel_buffer);
-
-        let se_tx = async move {
-            stderr_lines(child_stderr, stderr_tx)
-                .await
-                .map_err(|_| "se_tx fail")
-        };
-
-        let se_rx = async move {
-            while let Some(msg) = stderr_rx.recv().await {
-                f(msg);
-            }
-            Ok(())
-        };
-
-        (se_tx, se_rx)
-    });
+    let chn_o = fo.map(|(o, f)| gen_async_execution(channel_buffer, o.into(), f));
+    let chn_e = fe.map(|(e, f)| gen_async_execution(channel_buffer, e.into(), f));
 
     match (chn_o, chn_e) {
         (None, None) => {}
@@ -120,9 +160,59 @@ where
             tokio::try_join!(so_tx, so_rx)?;
         }
         (Some((so_tx, so_rx)), Some((se_tx, se_rx))) => {
-            tokio::try_join!(se_tx, se_rx, so_tx, so_rx)?;
+            tokio::try_join!(so_tx, so_rx)?;
+            tokio::try_join!(se_tx, se_rx)?;
         }
     };
 
     Ok(())
+}
+
+pub struct CmdExecutor {
+    stdout_fn: Option<Box<dyn Fn(String)>>,
+    stderr_fn: Option<Box<dyn Fn(String)>>,
+}
+
+impl CmdExecutor {
+    pub fn new() -> Self {
+        Self {
+            stdout_fn: None,
+            stderr_fn: None,
+        }
+    }
+
+    pub fn register_stdout_fn<F>(&mut self, f: F) -> &mut Self
+    where
+        F: Fn(String) + 'static,
+    {
+        self.stdout_fn = Some(Box::new(f));
+
+        self
+    }
+
+    pub fn register_stderr_fn<F>(&mut self, f: F) -> &mut Self
+    where
+        F: Fn(String) + 'static,
+    {
+        self.stderr_fn = Some(Box::new(f));
+
+        self
+    }
+
+    pub async fn exec(&self, channel_buffer: usize, arg: CmdArg<'_>) -> PqxResult<ExitStatus> {
+        let (mut child, child_stdout, child_stderr) = match arg {
+            CmdArg::Ping { addr } => gen_ping_cmd(addr)?,
+            CmdArg::Bash { cmd } => gen_bash_cmd(cmd)?,
+            CmdArg::Ssh { ip, cmd, user } => gen_ssh_cmd(ip, cmd, user)?,
+        };
+
+        exec_async_cmd(
+            channel_buffer,
+            self.stdout_fn.as_ref().map(|f| (child_stdout, f)),
+            self.stderr_fn.as_ref().map(|f| (child_stderr, f)),
+        )
+        .await?;
+
+        Ok(child.wait()?)
+    }
 }
--- pqx/tests/test_cmd.rs
+++ pqx/tests/test_cmd.rs
@@ -3,19 +3,38 @@
 //! date: 2023/05/22 23:15:04 Monday
 //! brief:
 
-use pqx::cmd::{async_cmd, gen_ping_cmd};
+use pqx::cmd::{gen_async_execution, gen_ping_cmd, CmdArg, CmdExecutor};
 
 fn print_stdout(s: String) {
     println!("print_stdout: {:?}", s);
 }
 
+fn print_stderr(s: String) {
+    println!("print_stderr: {:?}", s);
+}
+
 #[tokio::test]
-async fn test1() {
-    let (mut c, co, _) = gen_ping_cmd("github.com").unwrap();
+async fn cmd_compose_and_exec_success() {
+    let (_, co, _) = gen_ping_cmd("github.com").unwrap();
 
-    async_cmd(1, Some((co, print_stdout)), None).await.unwrap();
+    let (so_tx, so_rx) = gen_async_execution(1, co.into(), print_stdout);
 
-    println!("never reach");
+    let task = tokio::try_join!(so_tx, so_rx);
 
-    c.wait().unwrap();
+    println!("never reach!");
+    assert!(task.is_ok());
+}
+
+#[tokio::test]
+async fn cmd_executor_success() {
+    let mut executor = CmdExecutor::new();
+    executor.register_stdout_fn(print_stdout);
+    executor.register_stderr_fn(print_stderr);
+
+    let arg = CmdArg::Ping { addr: "github.com" };
+    let res = executor.exec(1, arg).await;
+
+    println!("never reach!");
+    assert!(res.is_ok());
+    println!("{:?}", res.unwrap());
 }
