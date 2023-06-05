6b81a0730866ed2344f463500c0b987b9852b83d
jacob-66.43 <jacobbishopxy@gmail.com>
2023-06-05 16:32:55 +0800

    :hatching_chick: add functions

--- pqx/src/mq/subscribe.rs
+++ pqx/src/mq/subscribe.rs
@@ -3,15 +3,23 @@
 //! date: 2023/05/26 23:55:05 Friday
 //! brief:
 
-use amqprs::channel::{BasicConsumeArguments, Channel};
+use amqprs::channel::*;
 use amqprs::consumer::AsyncConsumer;
-use amqprs::BasicProperties;
+use amqprs::{FieldName, FieldValue};
 use tokio::sync::Notify;
 
 use crate::error::PqxResult;
 
 // ================================================================================================
 // Subscriber
+//
+// methods:
+// 1. set_consume_args
+// 2. set_consumer_prefetch
+// 3. set_consumer_priorities
+// 4. consume
+// 5. cancel_consume
+// 6. block
 // ================================================================================================
 
 #[derive(Clone)]
@@ -20,8 +28,9 @@ where
     S: AsyncConsumer + Send + 'static,
 {
     channel: &'a Channel,
-    message_prop: BasicProperties,
+    consume_args: Option<BasicConsumeArguments>,
     consumer: Option<S>,
+    consumer_tag: Option<String>, // server generated tag, here we don't make it ourselves
 }
 
 impl<'a, S> Subscriber<'a, S>
@@ -31,13 +40,45 @@ where
     pub fn new(channel: &'a Channel, consumer: S) -> Self {
         Self {
             channel,
-            message_prop: BasicProperties::default(),
+            consume_args: Some(BasicConsumeArguments::default()),
             consumer: Some(consumer),
+            consumer_tag: None,
         }
     }
 
-    pub fn set_message_properties(&mut self, message_properties: BasicProperties) {
-        self.message_prop = message_properties;
+    pub fn set_consume_args(&mut self, consume_args: BasicConsumeArguments) {
+        self.consume_args = Some(consume_args);
+    }
+
+    pub async fn set_consumer_prefetch(
+        &self,
+        size: u32,
+        count: u16,
+        global: bool,
+    ) -> PqxResult<()> {
+        let args = BasicQosArguments::new(size, count, global);
+
+        self.channel.basic_qos(args).await?;
+
+        Ok(())
+    }
+
+    pub async fn set_consumer_priorities(&mut self, priority: i16) -> PqxResult<()> {
+        let x_priority = FieldName::try_from("x-priority").unwrap();
+
+        // no matter whether "x-priority" exists, remove it and insert a new one
+        self.consume_args
+            .as_mut()
+            .unwrap()
+            .arguments
+            .remove(&x_priority);
+
+        self.consume_args.as_mut().unwrap().arguments.insert(
+            FieldName::try_from("x-priority").unwrap(),
+            FieldValue::s(priority),
+        );
+
+        Ok(())
     }
 
     pub async fn consume(&mut self, que: &str) -> PqxResult<()> {
@@ -46,9 +87,34 @@ where
         } else {
             return Err("consumer is empty".into());
         };
-        let args = BasicConsumeArguments::new(que, "");
 
-        self.channel.basic_consume(consumer, args).await?;
+        let args = self
+            .consume_args
+            .take()
+            .unwrap()
+            .queue(que.to_owned())
+            .finish();
+
+        // start to consume
+        let consumer_tag = self.channel.basic_consume(consumer, args).await?;
+        // save consumer tag
+        self.consumer_tag = Some(consumer_tag);
+
+        Ok(())
+    }
+
+    pub async fn cancel_consume(&mut self, no_wait: bool) -> PqxResult<()> {
+        let consumer_tag = if let Some(ct) = self.consumer_tag.take() {
+            ct
+        } else {
+            return Err("consumer tag is empty, check whether consuming starts".into());
+        };
+        let args = BasicCancelArguments {
+            consumer_tag,
+            no_wait,
+        };
+
+        self.channel.basic_cancel(args).await?;
 
         Ok(())
     }
